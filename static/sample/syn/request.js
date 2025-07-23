0d59b598c4e64a5d24a5814e1bfae5591b404ec0
junworks77 <junworks77@gmail.com>
2025-07-23 12:57:36 +0900

    미사용중인 기본 모듈 정리

--- static/sample/syn/request.js
+++ static/sample/syn/request.js
@@ -1,23 +1,39 @@
-﻿$w.initializeScript({
-    btnQuery_click() {
-        syn.$r.params['p1'] = 'aaa';
-        syn.$r.params['p2'] = 'bbb';
-        syn.$r.params['p3'] = 'ccc';
-        syn.$l.get('txtQuery').value = syn.$r.query('p2');
+﻿'use strict';
+let $requests = {
+    extends: [
+        'parsehtml'
+    ],
+
+    method: {
     },
-    btnUrl_click() {
-        syn.$r.params['p1'] = 'aaa';
-        syn.$r.params['p2'] = 'bbb';
-        syn.$r.params['p3'] = 'ccc';
-        syn.$l.get('txtUrl').value = syn.$r.url();
-    },
-    btnSetCookie_click() {
-        syn.$r.setCookie('txtSetCookie', 'hello');
-    },
-    btnGetCookie_click() {
-        syn.$l.get('txtGetCookie').value = syn.$r.getCookie('txtSetCookie');
-    },
-    btnDeleteCookie_click() {
-        syn.$r.deleteCookie('txtSetCookie');
-    },
-})
+
+    event: {
+        btn_query_click() {
+            syn.$r.params['p1'] = 'aaa';
+            syn.$r.params['p2'] = 'bbb';
+            syn.$r.params['p3'] = 'ccc';
+            syn.$l.get('txt_query').value = syn.$r.query('p2');
+        },
+
+        btn_url_click() {
+            syn.$r.params['p1'] = 'aaa';
+            syn.$r.params['p2'] = 'bbb';
+            syn.$r.params['p3'] = 'ccc';
+            syn.$l.get('txt_url').value = syn.$r.url();
+        },
+
+        btn_setCookie_click() {
+            syn.$r.setCookie('txtSetCookie', 'hello');
+            syn.$l.get('txt_setCookie').value = '설정 완료';
+        },
+
+        btn_getCookie_click() {
+            syn.$l.get('txt_getCookie').value = syn.$r.getCookie('txtSetCookie');
+        },
+
+        btn_deleteCookie_click() {
+            syn.$r.deleteCookie('txtSetCookie');
+            syn.$l.get('txt_deleteCookie').value = '삭제 완료';
+        }
+    }
+};
